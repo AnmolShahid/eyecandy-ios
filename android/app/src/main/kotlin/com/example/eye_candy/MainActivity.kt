d768c5f0ee3f63389d42a86410f670aad8ad918c
anmolshahhid@hotmail.com <anmolshahid@hotmail.com>
2021-10-04 00:23:10 +0500

    update

--- android/app/src/main/kotlin/com/example/eye_candy/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ytrex.eye_candy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/ytrex/eye_candy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ytrex.eye_candy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
